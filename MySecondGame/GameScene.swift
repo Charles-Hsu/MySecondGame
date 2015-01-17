715873fc5b32eb70afce1558bcaadf102bfa0bc3
Charles Hsu <charles@6e.com.tw>
2015-01-17 13:07:07 +0800

    first commit

--- MySecondGame/GameScene.swift
+++ MySecondGame/GameScene.swift
@@ -9,37 +9,73 @@
 import SpriteKit
 
 class GameScene: SKScene {
-    override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-        
-        self.addChild(myLabel)
-    }
+
+  let heroSprite = SKSpriteNode(imageNamed: "Spaceship")
+  let invisibleControllerSprite = SKSpriteNode()
+
+  override func didMoveToView(view: SKView) {
     
-    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
-        /* Called when a touch begins */
-        
-        for touch: AnyObject in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
+    /* Setup your scene here */
+    heroSprite.xScale = 0.15
+    heroSprite.yScale = 0.15
+    heroSprite.position = CGPoint(x: frame.width/2, y: frame.height/2)
+    addChild(heroSprite)
+    
+    // Define invisible sprite for rotating and steering behavior without trigonometry
+    invisibleControllerSprite.size = CGSizeMake(0, 0)
+    addChild(invisibleControllerSprite)
+    
+    // Define a constraint for the orientation behavior
+    let rangeForOrientation = SKRange(constantValue: CGFloat(M_2_PI * 7))
+    
+    // M_2_PI == 2 / π == 0.636619772367581
+    
+    heroSprite.constraints = [SKConstraint.orientToNode(
+      invisibleControllerSprite, offset: rangeForOrientation)]
+  }
+  
+  override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+    /* Called when a touch begins */
+    
+    for touch: AnyObject in touches {
+      
+      // Determine the new position for the invisible sprite:
+      // The calculation is needed to ensure the position of both sprites
+      // are nearly the same, but different. Otherwise, the hero sprite rotates
+      // back to it's original orientation after reaching the location of 
+      // the invisible sprite.
+      
+      var xOffset: CGFloat = 1.0
+      var yOffset: CGFloat = 1.0
+      
+      var location = touch.locationInNode(self)
+      
+      if location.x > heroSprite.position.x {
+        xOffset = -1.0
+      }
+      
+      if location.y > heroSprite.position.y {
+        yOffset = -1.0
+      }
+      
+      // Create an action to move the invisibleControllerSprite.
+      // This will cause automatic orientation changes for the hero sprite
+      
+      let actionMoveInvisibleNode = SKAction.moveTo(
+        CGPointMake(location.x - xOffset, location.y - yOffset),
+        duration: 0.2)
+      
+      invisibleControllerSprite.runAction(actionMoveInvisibleNode)
+      
+      // Create an action to move the hero sprite to the touch location
+      
+      let actionMove = SKAction.moveTo(location, duration: 1)
+      heroSprite.runAction(actionMove)
+      
     }
-   
-    override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
-    }
+  }
+  
+  override func update(currentTime: CFTimeInterval) {
+    /* Called before each frame is rendered */
+  }
 }
--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # MySecondGame
+Refernece [How to implement a space shooter with SpriteKit and SWIFT](http://stefansdevplayground.blogspot.de/2014/11/how-to-implement-space-shooter-with.html)
+
+```swift
+// Detect the screen size
+let sizeRect = UIScreen.mainScreen().applicationFrame
+let width = sizeRect.size.width * UIScreen.mainScreen().scale
+let height = sizeRect.size.height * UIScreen.mainScreen().scale
+
+// Scene should be shown in fullscreen mode
+let scene = GameScene(size: CGSize(width: width, height: height))
+```
+
+```swift
+// Limit the supported screen orientation to LandscapeLeft
+override func supportedInterfaceOrientations() -> Int {
+  return Int(UIInterfaceOrientationMask.LandscapeLeft.rawValue)
+}
+```
+
+How ever, you alos need to change the Device Orientation of Deployment Info in the General Tab of the TARGETS, and modified the Info.plist to remove the items of the Supported interface orientations(iPad).
+
+So far so good, 
+
+degree to radians
+
+[](http://www.teacherschoice.com.au/Maths_Library/Angles/Angles14.gif)
+
+radians to degree
+
+[](http://www.teacherschoice.com.au/Maths_Library/Angles/Angles15.gif)
